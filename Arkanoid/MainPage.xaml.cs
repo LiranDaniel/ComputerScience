28bd627114025ca40515ec78fe42b9a9aba3bc16
Liran Daniel <118622291+LiranDaniel@users.noreply.github.com>
2023-11-15 23:42:06 +0200

    Copy the files

--- Arkanoid/MainPage.xaml.cs
+++ Arkanoid/MainPage.xaml.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +27,21 @@ namespace Arkanoid
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void btn_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Button btnPlayEnter = (Button)sender;
+            ((Image)btnPlayEnter.Content).Source = new BitmapImage(new Uri("ms-appx:/// " +
+                "Assets/App/Buttons/LIstOfButtons/" + ((Image)btnPlayEnter.Content).Name.Replace("img", "") + " (2).png"));
+            Window.Current.CoreWindow.PointerCursor = new Windows.UI.Core.CoreCursor(Windows.UI.Core.CoreCursorType.Hand, 1);
+        }
+        private void btn_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Button btnPlayExit = (Button)sender;
+            ((Image)btnPlayExit.Content).Source = new BitmapImage(new Uri("ms-appx:/// " +
+                "Assets/App/Buttons/LIstOfButtons/" + ((Image)btnPlayExit.Content).Name.Replace("img", "") + " (1).png"));
+            Window.Current.CoreWindow.PointerCursor = new Windows.UI.Core.CoreCursor(Windows.UI.Core.CoreCursorType.Arrow, 1);
         }
     }
 }
