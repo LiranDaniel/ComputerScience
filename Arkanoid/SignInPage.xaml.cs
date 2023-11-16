80c185104c0b662d6a691c74b52f9bb6bef7322c
Liran Daniel <118622291+LiranDaniel@users.noreply.github.com>
2023-11-16 10:08:41 +0200

    adding new pages (sigin in and play)

--- Arkanoid/SignInPage.xaml.cs
+++ Arkanoid/SignInPage.xaml.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,20 +14,19 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Arkanoid
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class SignInPage : Page
     {
-        public MainPage()
+        public SignInPage()
         {
             this.InitializeComponent();
         }
-
         private void btn_PointerEntered(object sender, PointerRoutedEventArgs e)
         {
             Button btnPlayEnter = (Button)sender;
@@ -45,14 +42,9 @@ namespace Arkanoid
             Window.Current.CoreWindow.PointerCursor = new Windows.UI.Core.CoreCursor(Windows.UI.Core.CoreCursorType.Arrow, 1);
         }
 
-        private void btnExit_Click(object sender, RoutedEventArgs e)
+        private void btn_SliderVertical_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Exit();
-        }
-
-        private void btnPlay_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GamePage));
+            Frame.Navigate(typeof(MainPage));
         }
     }
 }
