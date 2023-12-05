9774d50dfaa13b52e5745e5d024d1e4a25da941d
liran daniel <liranda6666@gmail.com>
2023-12-05 14:09:36 +0200

    Adding Constants class

---
+++ GameEngine/GameServices/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.GameServices
+{
+    public static class Constants
+    {
+        public const int RunInterval = 100;
+        public enum GameState
+        {
+            Loaded,
+            Started,
+            Paused,
+            GameOver
+        }
+
+    }
+}
