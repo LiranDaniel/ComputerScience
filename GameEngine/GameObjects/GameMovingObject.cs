887a51e5b1e79ef7e0574ed60d5ce94d58b42655
liran daniel <liranda6666@gmail.com>
2023-12-05 14:51:40 +0200

    ...

---
+++ GameEngine/GameObjects/GameMovingObject.cs
@@ -0,0 +1,18 @@
+﻿using GameEngine.GameServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.GameObjects
+{
+    /// <summary>
+    /// The class is a part of base for any object that can move
+    /// </summary>
+    public abstract class GameMovingObject : GameObject
+    {
+        protected GameMovingObject(Scene scene, string filename, double placeX, double placeY) :
+            base(scene, filename, placeX, placeY) { }
+    }
+}
