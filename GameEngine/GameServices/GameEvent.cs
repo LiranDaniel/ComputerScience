36f76706b04b393e5a71aa2cc5ee7f1b6632c8a9
liran daniel <liranda6666@gmail.com>
2023-12-05 13:45:50 +0200

    adding a function into Manager class

---
+++ GameEngine/GameServices/GameEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace GameEngine.GameServices
+{
+    public class GameEvent
+    {
+        public Action OnRun;                        // זהו אירוע אשר אחראי על התנועה
+        public Action OnClock;                      // זהו אירוע אשר אחראי להפעיל את השעון של העיקוב
+        public Action<VirtualKey> OnKeyUp;          // האירוע אשר אחראי לעזיבת המקש
+        public Action<VirtualKey> OnKeyDown;        // האירוע אשר אחראי לחיצת המקש
+    }
+}
--- GameEngine/GameServices/Manager.cs
+++ GameEngine/GameServices/Manager.cs
@@ -1,18 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Contacts;
+using Windows.UI.Xaml;
 
 namespace GameEngine.GameServices
 {
     public abstract class Manager
     {
         public Scene Scene { get; private set; }
+        private static DispatcherTimer _runTimer;                           // הטיימר יפעל ללא הפסקה
+
+        public static GameEvent GameEvent { get; } = new GameEvent();       // יצירת אירוע חדש ממחלקה GameEvent
+        private static DispatcherTimer _clockTimer;
 
         public Manager(Scene scene)
         {
             Scene = scene;
+
+            if(_runTimer == null)
+            {
+                _runTimer = new DispatcherTimer();
+                _runTimer.Interval = TimeSpan.FromMilliseconds(10);
+                _runTimer.Tick += _runTimer_Tick;
+                _runTimer.Start();
+            }
+        }
+
+
+
+        /// <summary>
+        /// the function is execute in frequency that never stop and turning an event that named OnRun
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _runTimer_Tick(object sender, object e)
+        {
+            if(GameEvent.OnRun != null)
+            {
+                GameEvent.OnRun();
+            }
         }
     }
 }
