52db499907f0fb57b4f58117b56e73e3c890c275
Liran Daniel <118622291+LiranDaniel@users.noreply.github.com>
2023-12-07 15:19:58 +0200

    adding jellj

--- Arkanoid/GameServices/GameManager.cs
+++ Arkanoid/GameServices/GameManager.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation.Metadata;
 using Windows.Globalization.DateTimeFormatting;
+using Windows.UI.Xaml;
 
 namespace Arkanoid.GameServices
 {
@@ -19,8 +20,15 @@ namespace Arkanoid.GameServices
         public void Init()
         {
             Scene.RemoveAllObjects();
-            var jelly = new Jelly(Scene, Jelly.JellyType.yellow, 200, 200, 300);
-            Scene.AddObject(jelly); 
+            var jelly = new Jelly(Scene, Jelly.JellyType.yellow, 100, 1, 1);
+            Scene.AddObject(jelly);
+            jelly = new Jelly(Scene, Jelly.JellyType.pink, 100, 100, 1);
+            Scene.AddObject(jelly);
+            jelly = new Jelly(Scene, Jelly.JellyType.green, 100, 200, 1);
+            Scene.AddObject(jelly);
+
+            //var bar = new Bar(Scene, "file", speed: 3, width: 160, Scene.ActualWidth / 2 - 80, Scene.Ground);
+            //Scene.AddObject(bar);
         }
     }
 }
