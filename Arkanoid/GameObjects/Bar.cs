bdf366d932f92fe0bfeaea0a4caf19d0f98b3874
Liran Daniel <118622291+LiranDaniel@users.noreply.github.com>
2023-12-07 15:11:57 +0200

    adding Rende func override

--- Arkanoid/GameObjects/Bar.cs
+++ Arkanoid/GameObjects/Bar.cs
@@ -5,20 +5,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
 using Windows.System;
 
 namespace Arkanoid.GameObjects
 {
     public class Bar : GameMovingObject
     {
-        public Bar(Scene scene, string filename, double placeX, double placeY) : base(scene, filename, placeX, placeY)
+        double _speed;
+        public Bar(Scene scene, string filename, double placeX, double placeY, double speed) : base(scene, filename, placeX, placeY)
         {
+            _speed = speed;
 
-                
+
         }
         private void KeyDown(VirtualKey key)
         {
-
+            switch (key)
+            {
+                case VirtualKey.Left:
+                    MoveTo(int.MinValue, _Y, _speed, 0);
+                    break;
+                case VirtualKey.Right:
+                    MoveTo(int.MinValue, _Y, _speed, 0);
+                    break;
+            }
+        }
+        public override void Render()
+        {
+            base.Render();
+            if (_X <= 0)
+            {
+                _X = 0;
+                Stop();
+            }
+            else if(_X >= _scene?.ActualWidth - width)
+            {
+                _X = _scene.ActualWidth - width;
+                Stop();
+            }
         }
     }
 }
