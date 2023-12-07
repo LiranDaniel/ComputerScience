a492287faaa32eed65f1064d4f1d97172e6bb732
Liran Daniel <118622291+LiranDaniel@users.noreply.github.com>
2023-12-07 15:07:47 +0200

    Creating bar class

---
+++ Arkanoid/GameObjects/Bar.cs
@@ -0,0 +1,24 @@
+﻿using GameEngine.GameObjects;
+using GameEngine.GameServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace Arkanoid.GameObjects
+{
+    public class Bar : GameMovingObject
+    {
+        public Bar(Scene scene, string filename, double placeX, double placeY) : base(scene, filename, placeX, placeY)
+        {
+
+                
+        }
+        private void KeyDown(VirtualKey key)
+        {
+
+        }
+    }
+}
