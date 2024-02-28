31a1d2d1904952fcffcc736db8ebe6982b59328f
liran daniel <liranda6666@gmail.com>
2024-02-28 20:57:14 +0200

    adding care images

--- BussinesTourProject/Pages/GamePage.xaml.cs
+++ BussinesTourProject/Pages/GamePage.xaml.cs
@@ -59,7 +59,7 @@ namespace BussinesTourProject.Pages
                                                22, 23, 24, 25, 26, 27, 28,
                                                29, 30, 31, 32, 33, 34, 35, 36 ,37,
                                                38 ,39},
-                                               { 5, 5, 6, 7, 8, 9, 5, 5, 5, 5, 5,
+                                               { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 12,
                                                11, 12, 13, 14, 15, 16, 17, 18, 19,
                                                20, 21, 22, 23, 24, 25, 26, 27, 28,
                                                29, 30, 31, 32, 33, 34, 35, 36 ,37,
@@ -100,6 +100,8 @@ namespace BussinesTourProject.Pages
                 {
                     currentPosition -= player.PlayerPosition.GetLength(1);
                 }
+                if (currentPosition == 11)
+                    player.Img.Source = new BitmapImage(new Uri("ms-appx:/// "));
                 Grid.SetRow(player.Img, player.PlayerPosition[0, currentPosition]);
                 Grid.SetColumn(player.Img, player.PlayerPosition[1, currentPosition]);
 
