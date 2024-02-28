b2d107be1e4d0ffb1ed0120a81e0e4dd4826904a
liran daniel <liranda6666@gmail.com>
2024-02-28 22:25:29 +0200

    chaing size of the car

--- BussinesTourProject/Pages/GamePage.xaml.cs
+++ BussinesTourProject/Pages/GamePage.xaml.cs
@@ -55,7 +55,7 @@ namespace BussinesTourProject.Pages
 
             //int[,] MatrixPositionPlayer1 = { { 10, 20, 30, 40, 50 }, { 5, 5, 5, 5, 5 } };
 
-            int[,] MatrixPositionPlayer1 = { { 82, 71, 64, 57, 51, 44, 38, 31, 25, 18, 9,
+            int[,] MatrixPositionPlayer1 = { { 81, 70, 63, 56, 50, 43, 37, 30, 24, 17, 9,
                                                8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
                                                22, 23, 24, 25, 26, 27, 28,
                                                29, 30, 31, 32, 33, 34, 35, 36 ,37,
