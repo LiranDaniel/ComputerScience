38bce4349bcadbccfb2e1af4613f319e991022bf
liran daniel <liranda6666@gmail.com>
2024-02-29 18:33:03 +0200

    ....

--- BussinesTourProject/Pages/GamePage.xaml.cs
+++ BussinesTourProject/Pages/GamePage.xaml.cs
@@ -117,11 +117,10 @@ namespace BussinesTourProject.Pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
 
-            int[,] MatrixPositionPlayer1 = { { 81, 70, 63, 56, 50, 43, 37, 30, 24, 17, 9,
-                                               8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-                                               22, 23, 24, 25, 26, 27, 28,
-                                               29, 30, 31, 32, 33, 34, 35, 36 ,37,
-                                               38 ,39},
+            int[,] MatrixPositionPlayer1 = { { 81, 70, 63, 56, 50, 43, 37, 30, 24, 17,
+                                               9, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+                                               17, 24, 30, 37, 43, 50, 56, 63, 70, 81,
+                                               81, 81, 81, 81, 81, 81, 81 ,81, 81 ,81},
                                                { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 12,
                                                11, 12, 13, 14, 15, 16, 17, 18, 19,
                                                20, 21, 22, 23, 24, 25, 26, 27, 28,
@@ -129,8 +128,8 @@ namespace BussinesTourProject.Pages
                                                38 ,39 } };
 
             InitPlayer(Player1, imgPlayer, MatrixPositionPlayer1);
-            gameManager = new GameManager();
-            gameManager.Start();
+            //gameManager = new GameManager();
+            //gameManager.Start();
         }
     }
 }
