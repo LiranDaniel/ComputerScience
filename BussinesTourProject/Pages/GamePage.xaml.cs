dab8d7a5e61bf3871f48d228b699e5517af05920
liran daniel <liranda6666@gmail.com>
2024-03-03 23:12:16 +0200

    ...

--- BussinesTourProject/Pages/GamePage.xaml.cs
+++ BussinesTourProject/Pages/GamePage.xaml.cs
@@ -161,7 +161,10 @@ namespace BussinesTourProject.Pages
 
         private async void MoveToJail(Player player)
         {
+            GridCards.Visibility = Visibility.Visible;
             await Task.Delay(TimeSpan.FromSeconds(2));
+            GridCards.Visibility = Visibility.Collapsed
+                ;
             player.currentPosition = 8;
             currentPlayer.Img.Source = new BitmapImage(new Uri($"ms-appx:///Assets/Images/Players/" + currentPlayer.name + "/RedCarRight.png"));
             Grid.SetColumnSpan(currentPlayer.Img, 5);
