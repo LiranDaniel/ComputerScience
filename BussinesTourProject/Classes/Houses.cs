94cc65e676960bcbe89c291ad8bef88d7e50fbd6
liran daniel <liranda6666@gmail.com>
2024-03-07 14:17:06 +0200

    ...

---
+++ BussinesTourProject/Classes/Houses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace BussinesTourProject.Classes
+{
+    public class Houses
+    {
+        public enum Level
+        {
+            Ground = 0,
+            House = 1,
+            villa = 2, 
+            Hotel = 3
+        }
+        public static Dictionary<Level, string> filePathImages = new Dictionary<Level, string>();
+        public Image HouseImage;
+        public int position;
+    }
+}
