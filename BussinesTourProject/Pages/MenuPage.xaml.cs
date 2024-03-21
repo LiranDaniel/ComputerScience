103d2ea24bd58010217a65565271534a337a553f
liran daniel <liranda6666@gmail.com>
2024-03-21 14:24:05 +0200

    ....

--- BussinesTourProject/Pages/MenuPage.xaml.cs
+++ BussinesTourProject/Pages/MenuPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Gaming.Input;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -52,6 +53,7 @@ namespace BussinesTourProject.Pages
 
         private void btnHome_Click(object sender, RoutedEventArgs e)
         {
+            string dbPath = ApplicationData.Current.LocalFolder.Path;
             Frame.Navigate(typeof(GamePage));
         }
 
