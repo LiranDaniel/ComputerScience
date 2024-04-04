e2910af415ba8ca62224a2e40afea20b9235e7e5
liran daniel <liranda6666@gmail.com>
2024-04-04 11:40:21 +0300

    adding data base refrence

---
+++ BussinesTourProject/Classes/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesTourProject.Classes
+{
+    public class GameManager
+    {
+        public static DataBase.Models.User User { get; set; }
+    }
+}
