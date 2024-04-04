82829587170e5155a44f29f9a3479f2dd95d1e99
liran daniel <liranda6666@gmail.com>
2024-04-04 13:00:32 +0300

    adding server class

---
+++ DataBase/Server.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace DataBase
+{
+    public static class Server
+    {
+        private static string dbPath = ApplicationData.Current.LocalFolder.Path;
+        private static string connectString = "Filename=" + dbPath + "\\DBGame.db";
+
+        public static int? ValidateUser(string userName, string userPassword)
+        {
+            string query = $"SELECT UserId FROM [User] WHERE UserName='{userName}' AND UserPassword='{userPassword}'";
+            using (SqliteConnection connection = new SqliteConnection(connectString))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(query, connection);
+                SqliteDataReader reader = command.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    reader.Read();
+                    return reader.GetInt32(0);
+                }
+                return null;
+            }
+        }
+
+
+    }
+}
