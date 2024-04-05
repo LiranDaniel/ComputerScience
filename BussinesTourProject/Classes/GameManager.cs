40354a7269a0159f494c6cbb19eee97e9ca99866
liran daniel <liranda6666@gmail.com>
2024-04-05 16:40:41 +0300

    deleting the refrence of the players

--- BussinesTourProject/Classes/GameManager.cs
+++ BussinesTourProject/Classes/GameManager.cs
@@ -9,15 +9,14 @@ namespace BussinesTourProject.Classes
     public static class GameManager
     {
         public static Random rnd = new Random();
-        public static Player Player1 = new Player(name: "MainPlayer");
-        public static Player Player2 = new Player(name: "Player2");
-        public static Player Player3 = new Player(name: "Player3");
-        public static Player Player4 = new Player(name: "Player4");
 
         public static Player currentPlayer;
         public static int currentTimesPlay = 1;
         private static int IndexPlayers = rnd.Next(0, 4);
-        public static Player[] arrayPlayers = { Player1, Player2, Player3, Player4 };
+        public static Player[] arrayPlayers = { new Player(name: "MainPlayer"),
+            new Player(name: "Player2"),
+            new Player(name: "Player3"),
+            new Player(name: "Player4")};
 
         public static int[,] MatrixPositionPlayer1 = { { 79, 69, 61, 53, 45, 37, 29, 21,
                                                          0, 8, 8, 8, 8, 8, 8, 8,
