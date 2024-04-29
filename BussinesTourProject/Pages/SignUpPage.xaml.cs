c2bc4a4e899c3f55f368e44f4a4fd6848f4a088c
liran daniel <liranda6666@gmail.com>
2024-04-29 05:38:23 +0300

    adding database

--- BussinesTourProject/Pages/SignUpPage.xaml.cs
+++ BussinesTourProject/Pages/SignUpPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataBase;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,6 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using DataBase;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -50,13 +52,9 @@ namespace BussinesTourProject.Pages
             Frame.Navigate(typeof(MenuPage));
         }
 
-        private void btnSignIn_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
         private void btnSignUp_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(GamePage));
+            Server.SignUp(tboxMail, tboxName, tboxPassword);
         }
 
 
--- DataBase/Server.cs
+++ DataBase/Server.cs
@@ -6,13 +6,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
 
 namespace DataBase
 {
     public static class Server
     {
+
         private static string dbPath = ApplicationData.Current.LocalFolder.Path;
         private static string connectString = "Filename=" + dbPath + "\\DBGame.db";
+
+
+        private async static void PlayErrorSound()
+        {
+            MediaElement soundPlayer = new MediaElement();
+            soundPlayer.Source = new Uri("ms-appx:///Assets/Music/sad-hamster.wav"); // Replace with your audio file path
+            soundPlayer.Play();
+        }
+
+        public async static void SignUp(TextBox mailTestBlox,TextBox nameTestBlox, PasswordBox passwordTextBlox)
+        {
+            string email = mailTestBlox.Text;
+            string password = passwordTextBlox.Password;
+            string name = nameTestBlox.Text;
+            // Connect to SQLite database
+            using (SqliteConnection connection = new SqliteConnection(connectString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Check if the user already exists
+                    string query = "SELECT COUNT(*) FROM User WHERE UserMail = @Email";
+                    using (SqliteCommand command = new SqliteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Email", email);
+                        int count = Convert.ToInt32(command.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                            // User already exists, show error message
+                            var dialog = new MessageDialog("User with this email already exists.");
+                            PlayErrorSound();
+                            await dialog.ShowAsync();
+                            return; // Exit the method
+                        }
+                    }
+
+                    // User doesn't exist, proceed with signup
+                    string insertCommand = $"INSERT INTO [User] (UserName,UserPassword,UserMail) VALUES ('{name}','{password}','{email}')";
+                    using (SqliteCommand command = new SqliteCommand(insertCommand, connection))
+                        command.ExecuteNonQuery();
+
+                }
+                catch (SqliteException ex)
+                {
+                    // Handle SQLite exceptions
+                    var dialog = new MessageDialog("SQLite Error: " + ex.Message);
+                    PlayErrorSound();
+                    await dialog.ShowAsync();
+                }
+                catch (Exception ex)
+                {
+                    // Handle other exceptions
+                    var dialog = new MessageDialog("Error: " + ex.Message);
+                    PlayErrorSound();
+                    await dialog.ShowAsync();
+                }
+            }
+        }
+
+
+
+
+
 
         /*
         //הפעולה מבצעת שאילתה
