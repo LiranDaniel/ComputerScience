806810e6870851daf4e3f15eea571b3f56919537
liran daniel <liranda6666@gmail.com>
2024-05-08 18:18:14 +0300

    ...

--- BussinesTourProject/Pages/SignUpPage.xaml.cs
+++ BussinesTourProject/Pages/SignUpPage.xaml.cs
@@ -15,7 +15,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-using DataBase;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
